5f1fa4fce47b138d8785658bc1ea12db03b82d49
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-01-02 22:12:44 +0200

    SCSS

--- 21.12/script.js
+++ 21.12/script.js
@@ -78,28 +78,28 @@
 
 
 // Write a JavaScript for loop that will iterate from 0 to 15.
-//For each iteration, it will check if the current number is odd or even,
-//and display a message to the screen. 
+// For each iteration, it will check if the current number is odd or even,
+// and display a message to the screen. 
 // Sample Output :
-// "0 is even"
-// "1 is odd"
-// "2 is even"
-// for ( let i = 0; i <= 15; i++ ){
-//     if ( i % 2 == 0 ){
-//         console.log(`${i} is even`)
-//     } else {
-//         console.log(`${i} is odd`)
-//     }
-// }
+"0 is even"
+"1 is odd"
+"2 is even"
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} is even`)
+    } else {
+        console.log(`${i} is odd`)
+    }
+}
 
 
 // Write a JavaScript program to construct the following pattern,
-//using a nested for loop. 
-// *  
-// * *  
-// * * *  
-// * * * *  
-// * * * * * 
+//using a nested for loop.
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
 // let string = '*';
 // let i;
 // let j;
