8441a91d5b32f9735f883a30ef463a6faac963b8
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-01-04 22:24:30 +0200

    change

--- 2.01.22/angular/project/src/app/app.module.ts
+++ 2.01.22/angular/project/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    NavBarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
