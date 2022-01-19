e52237357c82d064057a49287a1e806b3145ad72
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-01-19 22:44:41 +0200

    new

--- 19.01.22/react/my-app/src/NewApp.js
+++ 19.01.22/react/my-app/src/NewApp.js
@@ -2,11 +2,29 @@ import React from 'react'
 
 function NewApp() {
 
+function user(obj) {
+    return `${obj.name} is from ${obj.city} and she is ${obj.age} years old`
+}
+
+function adult(obj) {
+    if (obj) {
+        if (obj.name) {
+            return <p>{`${obj.name} is from ${obj.city} and she is ${obj.age} years old`}</p>
+        } else {
+            return <p>name is empty</p>
+        }
+    } else {
+        return <p>obj is empty</p>
+    }
+}
+
 const obj = {name: 'Golda', city: 'Holon', age: 27}
 
 return(
     <>
-    <p>{obj.name} is from {obj.city} and she is {obj.age} years old</p>
+    {/* <p>{obj.name} is from {obj.city} and she is {obj.age} years old</p> */}
+    {/* <p>{user(obj)}</p> */}
+    {adult()}
     </>
 )
 
