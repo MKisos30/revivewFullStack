ea0ac927e705c91bfcd75dfe4fa58b5ace5bfdb9
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-01-19 21:54:42 +0200

    Update App.js

--- 19.01.22/react/my-app/src/App.js
+++ 19.01.22/react/my-app/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+  const user = {
+    name:"Matilda",
+    hobbies:"walk"
+  }
+
+  const name = `Miriam`
+  const element = <h1>Hi {name}!</h1>
 
   return (
     <>
+    {element}
+    <div>{user.name} like {user.hobbies}</div>
     </>
   );
 }
