1bea0b3bbac7f5fa2dd1b22155afdf8147bb74b0
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2022-01-22 18:42:23 +0200

    ol

--- 20.01.22/props/src/Miriam/dist/App.dev.js
+++ 20.01.22/props/src/Miriam/dist/App.dev.js
@@ -1,9 +1,19 @@
+// import React from "react";
+// import Children from "./Children";
+// function App() {
+//     const children = [
+//         {name: 'Nechami', age: 12},
+//         {name: 'Esti', age: 9},
+//         {name: 'Pini', age: 6}
+//     ]
+//     return(
+//         <>
+//             <div>{children.map(item => 
+//                 <Children child = {item}/>
+//                 // <p>{item.name}</p>
+//                 ) }</div>
+//         </>
+//     )
+// }
+// export default App;
 "use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function App() {
-  return;
-}
--- 20.01.22/props/src/Miriam/dist/Children.dev.js
+++ 20.01.22/props/src/Miriam/dist/Children.dev.js
@@ -1,15 +1,13 @@
+// import React from "react";
+// import childrenStyle from './childerStyle.module.css'
+// function Children(props) {
+//     const child = props.child
+//     return (
+//         <div className={childrenStyle.info}>
+//             <h1 className={childrenStyle.name}>name: {child.name}</h1>
+//             <h2 className={childrenStyle.age}>age: {child.age}</h2>
+//         </div>
+//     )
+// }
+// export default Children;
 "use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-
-var _react = _interopRequireDefault(require("react"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function Children() {}
-
-var _default = Children;
-exports["default"] = _default;
