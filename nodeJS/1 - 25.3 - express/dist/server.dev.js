0fc69ca2a5b3ca2266207cadb706524a1af4a28f
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-03-25 15:25:48 +0300

    help

--- nodeJS/1 - 25.3 - express/dist/server.dev.js
+++ nodeJS/1 - 25.3 - express/dist/server.dev.js
@@ -7,6 +7,7 @@ var app = express();
 var color = require('colors');
 
 var port = process.env.PORT || 4040;
+app.use(express["static"]("public"));
 var tasks = [{
   id: "1",
   title: "one"
@@ -19,7 +20,14 @@ var tasks = [{
 }, {
   id: "4",
   title: "four"
-}];
+}]; // app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/public' + '/index1.html')
+// })
+
+app.get('/get-data', function (req, res) {
+  res.send(tasks);
+});
+console.log(color.bgCyan.italic.black('Miriam'));
 app.listen(port, function () {
   console.log(color.bgBlue.white("server listen on http://localhost:".concat(port)));
 });
