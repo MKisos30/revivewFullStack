7eae84986807e2ff11d609c014d26045231c27bc
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-03-19 20:00:29 +0200

    grid

---
+++ BackToStart/JavaScript - Object/index.js
@@ -0,0 +1,37 @@
+// const obj = {
+//     key : value
+// }
+// const qweqew = 'qweqwe'
+// const assa = {qweqew}
+
+// console.log(assa)
+
+// const child = "haha"
+
+// const many = {
+//     getMyAge: function() {
+//         return this.age
+//     },
+//     name: "Miriam",
+//     age : 32,
+// }
+
+// console.log(many.getMyAge())
+
+// const arrShop = [{id:1, item:"shoes", price: 12}]
+
+// const shop = {
+//     itemsOfShop: arrShop,
+//     addToItems: function() {
+//         this.itemsOfShop.push({id:2, item: "watch", price: 30})
+//         return this.itemsOfShop
+//     }
+// }
+
+// console.log(shop.addToItems())
+
+
+
+// cuntinue the shop objecr
+// create function of remove item feom the arrShop
+// create function of edit item in arrshop
