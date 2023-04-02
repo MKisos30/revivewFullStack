1e2a636fbc41aa63e680a8ea5e761cbab6414bbf
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-04-02 20:26:11 +0300

    ex in function

---
+++ BackToStart/explain-to-Katya/ex3.js
@@ -0,0 +1,105 @@
+// const numFromUser = prompt("Enter a number")
+
+// const negativeNum = (numFromUser) => {
+//     if (numFromUser > 0) {
+//         return console.log(numFromUser * -1)
+//     } else {
+//         return console.log(numFromUser * -1)
+//     }
+// }
+
+// negativeNum(numFromUser);
+
+// *******************
+
+// const x = prompt('Enter number 1')
+// const y = prompt('Enter number 2')
+
+// const multiFun = (x, y) => {
+
+//     return console.log(x * y)
+// }
+
+// multiFun(x, y)
+
+
+// *******************
+
+// const numFromUser = prompt("Enter a number")
+
+// const sqaureFun = (numFromUser) => {
+
+//     return console.log(Math.sqrt(numFromUser)) 
+// }
+
+// sqaureFun(numFromUser)
+
+
+// *******************
+
+// const numFromUser = prompt("Enter a number") 
+
+// const weatherFun = (numFromUser) => {
+
+//     // * 1.8 + 32
+
+//     return console.log((numFromUser * 1.8) + 32)
+// }
+
+// weatherFun(numFromUser)
+
+
+// *******************
+
+// const numFromUser = prompt("Enter a number")
+
+// const checkNum = (numFromUser) => {
+//     if (numFromUser % 2 == 0) {
+//         return console.log('The number is even')
+//     } else {
+//         return console.log('The number is odd')
+//     }
+// }
+
+// checkNum(numFromUser)
+
+
+// *******************
+
+// const x = prompt('Enter number 1')
+// const y = prompt('Enter number 2')
+
+// const largeNum = (x, y) => {
+
+//     return console.log(Math.max(x, y))
+// }
+
+// largeNum(x, y)
+
+
+// *******************
+
+// const numFromUser = prompt("Enter a number")
+
+const reverseFun = (numFromUser) => {
+    
+    return console.log(numFromUser.split('').reverse().join(''))
+}
+
+reverseFun(numFromUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
