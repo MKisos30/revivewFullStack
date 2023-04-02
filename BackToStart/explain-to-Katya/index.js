ff41c25cbcf33800b8fc0561adceae5ef34ba1ae
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-04-02 19:03:35 +0300

    function ex

--- BackToStart/explain-to-Katya/index.js
+++ BackToStart/explain-to-Katya/index.js
@@ -1,7 +1,26 @@
-const numbersFromUser = prompt("Enter 6 random numbers")
+// const numbersFromUser = prompt("Enter 6 random numbers")
 
 const loto = (numbersFromUser) => {
-    return numbersFromUser
+    
+    const nums = numbersFromUser.split(" ")
+
+    const arrayRandom = []
+    for (let i = 0; i < 6; i++) {
+        let x = Math.floor((Math.random() * 37) + 1)
+        arrayRandom.push(x)
+    }
+
+    checkLoto(arrayRandom, nums)
 }
 
-console.log(numbersFromUser)
+const checkLoto = (arrayRandom, nums) => {
+    for (let i = 0; i < arrayRandom.length; i++) {
+        if(arrayRandom.includes(Number(nums[i]))) {
+            alert(`the number ${nums[i]} won`)
+        } else {
+            alert('no one won')
+        }
+    }
+}
+
+loto(numbersFromUser)
