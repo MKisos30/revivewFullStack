f50bc165337be666b5ce070018b2ba6018afe8e0
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-04-17 10:02:14 +0300

    ex4

---
+++ BackToStart/explain-to-Katya/ex4.js
@@ -0,0 +1,12 @@
+const item = [
+    {nameOfBuyer: "Oz"},
+    {item: "pence"},
+    {price: "25$"}
+] 
+
+
+const funcItem = (item) => {
+    const nameOfItem = prompt("enter your item name")
+
+    return 
+}
