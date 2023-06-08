04ce2baa0c6e66b3d113ab99ef0ff8da75f4d603
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2023-06-08 21:29:10 +0300

    typeScript

---
+++ TypeScript/Miriam/index.ts
@@ -0,0 +1,33 @@
+const showRandomNumber = document.querySelector("#showRandomNumber") as HTMLElement;
+
+const sayHello = () => {
+    return alert('Hello')
+}
+
+const sayHelloToUserName = (ev: Event) => {
+    ev.preventDefault()
+
+    // const userName = ev.target.elements.nameOfUser.value
+
+    const target = ev.target as typeof ev.target & {
+        nameOfUser: { value: string, id: number },
+        ageOfUser: { value: number }
+    }
+
+    const username = target.nameOfUser.value
+    const usernameID = target.nameOfUser.id
+    const ageOfUser = target.ageOfUser.value
+
+    console.log(username)
+    console.log(usernameID)
+    console.log(ageOfUser)
+}
+
+const getRandomNumber = (): number => {
+    return Math.random()
+}
+
+const showRandomNumberFunction = () => {
+    const randomNumber = getRandomNumber()
+    showRandomNumber.innerHTML = randomNumber.toString()
+}
